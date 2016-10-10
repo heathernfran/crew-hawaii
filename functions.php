4a0613b217c9a0ad47b5c891f7051ea591056f05
Heather Frantisak <heathernfran@gmail.com>
2016-10-09 20:19:08 -1000

    Enqueue basscss and semantic-ui css in functions. Use semantic-ui font

--- functions.php
+++ functions.php
@@ -2,5 +2,7 @@
 
 add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
 function enqueue_theme_styles() {
+  wp_enqueue_style('basscss', get_stylesheet_directory_uri() . '/node_modules/basscss/css/basscss.min.css');
+  wp_enqueue_style('semantic-ui-css', get_stylesheet_directory_uri() . 'node_modules/semantic-ui/dist/semantic.min.css');
   wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css');
 }
